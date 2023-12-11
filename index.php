4b8605e364b5f9ee5efef0310dd03350c71c392b
Diogo Timóteo <dtimoteo292@gmail.com>
2023-12-10 21:18:37 -0300

    index e navbar

--- index.php
+++ index.php
@@ -2,41 +2,46 @@
 <html>
     <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Página inicial</title>
         <link rel="stylesheet" type="text/css" href="css/navbar.css" />
     </head>
     <body>
         <div class="topnav">
-            <img src="images/logo.png" alt="Cabrum" style="width: 5%;" />
+            <img src="Images/logo.png" alt="Cabrum" style="width: 5%;" />
             <a href="Produtos/indexProdutos.php">Produtos</a>
             <a href="Pedidos/indexPedidos.php">Pedidos</a>
             <a href="Carrinho/indexCarrinho.php">Carrinho</a>
+            <a href="indexAdministrador.php">Administrador</a>
         </div>
-        <?php
-            include_once './funcoes/banco.php';
 
-            $conn = conectaBanco();
-            $sql = 'select * from categoria;';
-            $resultado = $conn->query($sql);
-        ?>
+        <div style="padding:30px;margin-top:50px;">
+            <?php
+                include_once './funcoes/banco.php';
 
-        <div><h2>Procure os produtos pela categoria</h2></div>
-        <?php
-        $i = 0;
+                $conn = conectaBanco();
+                $sql = 'select * from categoria;';
+                $resultado = $conn->query($sql);
+            ?>
 
-        while ($categoria = $resultado->fetch(PDO::FETCH_ASSOC)) {
-            $id_categoria = $categoria["id_categoria"];
-            $nm_categoria = $categoria["nm_categoria"];
+            <div><h2>Procure os produtos pela categoria</h2></div>
+            <?php
+            $i = 0;
 
-            if ($i % 5 == 0) {
-                echo "<br>";
+            while ($categoria = $resultado->fetch(PDO::FETCH_ASSOC)) {
+                $id_categoria = $categoria["id_categoria"];
+                $nm_categoria = $categoria["nm_categoria"];
+
+                if ($i % 5 == 0) {
+                    echo "<br>";
+                }
+
+                echo '<a style="padding:5px" href="Produtos/indexProdutos.php?id_categoria=' .
+                    $id_categoria . '">' . $nm_categoria . '</a>';
+
+                $i += 1;
             }
-
-            echo '<a style="padding:5px" href="Produtos/indexProdutos?id_categoria=' . $id_categoria . '">' . $nm_categoria . '</a>';
-
-            $i += 1;
-        }
-        ?>
-
+            ?>
+        </div>
     </body>
 </html>
---
+++ indexAdministrador.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="css/navbar.css" />
+    </head>
+    <body>
+        <div class="topnav">
+            <img src="Images/logo.png" alt="Cabrum" style="width: 5%;" />
+            <a href="Produtos/indexProdutos.php">Produtos</a>
+            <a href="Pedidos/indexPedidos.php">Pedidos</a>
+            <a href="Carrinho/indexCarrinho.php">Carrinho</a>
+            <a href="indexAdministrador.php" class="active">Administrador</a>
+        </div>
+        
+        <div style="padding:30px;margin-top:50px;">
+            <h2>Cadastrar</h2>
+            <a href="Transportadora/cadastrarTransportadora.php">Transportadora</a>
+            <a href="Categoria/cadastrarCategoria.php">Categoria</a>
+            <a href="Produtos/cadastrarProduto.php">Produto</a>
+
+            <h2>Editar/Remover</h2>
+            <a href="Transportadora/listaTrasportadora.php">Transportadora</a>
+            <a href="Categoria/listaCategoria.php">Categoria</a>
+            <a href="Produtos/listaProduto.php">Produto</a>
+        </div>
+    </body>
+</html>
